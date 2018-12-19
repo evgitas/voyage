7ff483dcb8db6c67e7b24a08d40782dcba051205
evgitas <evaldaska@gmail.com>
2018-12-19 09:13:12 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html> 
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>VOYAGE</title>
+	<link href="https://fonts.googleapis.com/css?family=Raleway|Roboto:300,400,500,700&amp;subset=latin-ext" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<!-- landing top start -->
+	<section class="top">
+		<div >
+			<div class="container flex-container containerheight40">
+				<div>
+					<ul class="flex-container">                        
+						<li><a href="#">Agent Login</a></li>
+						<li><a href="#">Custome Login</a></li>
+						<li><a href="#">Not a Member? <span> Register</span></a></li>
+						<li><a href="#">Call Us Now: 3705588445</a></li>
+					</ul>
+				</div>
+				<div>
+						<ul class="flex-container">
+							<li>
+								<a href="http://facebook.com" target="_blank">
+									<i class="fab fa-facebook-f"></i>
+								</a>
+							</li>
+							<li>
+								<a href="http://pinterest.com" target="_blank">
+									<i class="fab fa-pinterest-p"></i>
+								</a>
+							</li>
+							<li>
+								<a href="http://twitter.com" target="_blank">
+									<i class="fab fa-twitter"></i>
+								</a>
+							</li>
+							<li>
+								<a href="http://linkedin.com" target="_blank">
+									<i class="fab fa-linkedin-in"></i>
+								</a>
+							</li>
+						</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- navigation start -->
+	<section class="navigation">
+		<div class="container">
+			<div class="flex-container">
+				<div class="logo">V<span>O</span>YAGE</div>
+				<div class="flex-container main_navigation">
+					<div class="">
+						<ul class="flex-container">                        
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Destinations</a></li>
+						<li><a href="#">Cruises</a></li>
+						<li><a href="#">Special</a></li>
+						<li><a href="#">Holidays</a></li>
+						<li><a href="#">Blog</a></li>
+						</ul>
+					</div>
+					<div class="circle"><i class="fas fa-search"></i></div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Navigation end -->
+	<!-- Landing picture with searc start -->
+	<section class="landing_picture">
+		<div>
+			<div class="flex-container">
+				<div class="slogan_container">
+					<div class="container">
+						<div class="flex-container2">
+							<div>
+								<h1>To travel is to live</h1>
+							<!-- </div> -->
+							<!-- <div> -->
+								<h2>You Don't Need Magic to Disappear. All you need is a destination.</h2>
+							</div>
+							<div>
+								<a class="btn" href="#">Show on map</a>
+								<a class="btn" href="#">More info</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="search_container">
+					<div class="container">
+						find
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Landing picture with searc end -->
+
+
+</body>
+</html>
