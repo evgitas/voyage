bb328efd029334fcea016745ee9a4431fa10ba38
evgitas <evaldaska@gmail.com>
2019-01-02 09:36:11 +0200

    0102_1
    f

--- index.php
+++ index.php
@@ -6,10 +6,13 @@
 	<title>VOYAGE</title>
 	<link href="https://fonts.googleapis.com/css?family=Raleway|Roboto:300,400,500,700&amp;subset=latin-ext" rel="stylesheet">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<<<<<<< HEAD
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="assets/css/carousel.css">
 	<!-- Bootstrap  -->
 	<link rel="stylesheet" href="assets/css/bootstrap.css">
+=======
+>>>>>>> parent of aed4d65... karusele
 	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
@@ -22,7 +25,7 @@
 	<section class="top">
 		<div>
 			<div class="header-top">
-		      <div class="widthcontainer">
+		      <div class="container">
 		        <form action="">
 		          <div id="search">
 		          <input type="text" placeholder="Enter search keyword here ......" name="s" id="top_search" style="display: inline-block;">
@@ -33,7 +36,7 @@
 		        </form>
 		      </div>
 		    </div>
-			<div class="widthcontainer flex-container">
+			<div class="container flex-container">
 				<div>
 					<ul class="flex-container">                        
 						<li><a href="#">Agent Login</a></li>
@@ -71,7 +74,7 @@
 	</section>
 	<!-- navigation start -->
 	<section class="navigation">
-		<div class="widthcontainer">
+		<div class="container">
 			<div class="flex-container">
 				<div class="logo">V<span>O</span>YAGE</div>
 				<div class="flex-container main_navigation">
@@ -129,7 +132,7 @@
 		<div>
 			<div class="flex-container">
 				<div class="slogan_container">
-					<div class="widthcontainer">
+					<div class="container">
 						<div class="flex-container2">
 							<div>
 								<h1>To travel is to live</h1>
@@ -143,7 +146,7 @@
 					</div>
 				</div>
 				<div class="search_container">
-					<div class="widthcontainer">
+					<div class="container">
 						<form action="">
 						<div class="flex-container4">
 							<div class="search_content h7">FIND YOUR <br><span>HOLYDAYS</span></div>
@@ -160,108 +163,6 @@
 		</div>
 	</section>
 	<!-- Landing picture with searc end -->
-
-
-
-
-	<div class="widthcontainer">
-			<!-- sukuriame eilutes elementa, kuriame galesime sukurti stulpelius -->
-			<div class="row">
-				<!-- sukuriame stulpeli -->
-				<!-- prie dideliu ekranu norime rodyti po 4 stulpelius eiluteje -->
-				<!-- prie vidutiniu ekranu norime rodyto po 2 stulpelius eiluteje -->
-				<!-- prie mazesniu norime rodyti po 1 stulpeli eiluteje -->
-				<div class="col-12 col-md-6 col-lg-3">
-					<!-- sukuriame stulpelio turini -->
-					<h3>Pirmas paragrafas</h3>
-				</div>
-				<div class="col-12 col-md-6 col-lg-3">
-					<!-- sukuriame stulpelio turini -->
-					<h3>aNTRAS paragrafas</h3>
-				</div>
-				<div class="col-12 col-md-6 col-lg-3">
-					<!-- sukuriame stulpelio turini -->
-					<h3>Trecias paragrafas</h3>
-				</div>
-				<div class="col-12 col-md-6 col-lg-3">
-					<!-- sukuriame stulpelio turini -->
-					<h3>Kervirtas paragrafas</h3>
-				</div>
-			</div>
-	</div>
-
-
-
-	<!-- Offers test section start -->
-	<section id="offers" class="offers_test">
-		<!-- <div class="flex-container"> -->
-			<div class=""><h3>Special Offers</h3>
-						<h4>Best 2019 packages where people love to stay!</h4>
-			</div>
-			<div class="widthcontainer">
-				<div class="row">
-					<div class="col-6 col-md-4 col-lg-2 center">
-						<a href="#">
-						<div class="offer_content_test">
-							<img src="assets/images/offer1.jpg" alt="USA">
-							<h5>Statue of Liberty</h5>
-							<h5>New Destination!</h5>
-						</div>
-						</a>
-					</div>
-					<div class="col-6 col-md-4 col-lg-2 center">
-						<a href="#">
-						<div class="offer_content_test">
-							<a href="#"><img src="assets/images/offer2.png" alt="USA">
-							<h5>Spectacular Seychelles</h5>
-							<h5>New Destination!</h5>
-						</div>
-						</a>
-					</div>
-					<div class="col-6 col-md-4 col-lg-2 center">
-						<a href="#">
-						<div class="offer_content_test">
-							<img src="assets/images/offer3.png" alt="USA">
-							<h5>Hong Kong & Macau</h5>
-							<h5>Bonus Extras!</h5>
-						</div>
-						</a>
-					</div>
-					<div class="col-6 col-md-4 col-lg-2 center">
-						<a href="#">
-						<div class="offer_content_test">
-							<img src="assets/images/offer4.png" alt="USA">
-							<h5>Vietnam Highlights</h5>
-							<h5>New Destination!</h5>
-						</div>
-						</a>
-					</div>
-					<div class="col-6 col-md-4 col-lg-2 center">
-						<a href="#">
-						<div class="offer_content_test">
-							<img src="assets/images/offer5.png" alt="USA">
-							<h5>Spectacular Seychelles</h5>
-							<h5>New Destination!</h5>
-						</div>
-						</a>
-					</div>
-					<div class="col-6 col-md-4 col-lg-2 center">
-						<a href="#">
-						<div class="offer_content_test">
-							<img src="assets/images/offer6.png" alt="USA">
-							<h5>Dubai</h5>
-							<h5>New Destination!</h5>
-						</div>
-						</a>
-				</div>
-			</div>
-		<!-- </div> -->
-	</section>
-
-<!-- test end -->
-
-
-
 	<!-- Offers section start -->
 	<section id="offers" class="offers">
 		<!-- <div class="flex-container"> -->
@@ -306,7 +207,7 @@
 <!-- Holiday type start -->
 	<section id="holiday_type" class="holiday_type">
 		<div class="holiday_type_container">
-			<div class="widthcontainer">
+			<div class="container">
 				<div><h3>Holidays Type</h3><h4>get explore your dream to travel the world!</h4></div>
 				<div class="flex_container5">
 					<div class="holiday_type_content"><a href="#"><i class="fas fa-ship circle"></i><h4>Cruise</h4></a></div>
@@ -319,39 +220,6 @@
 			</div>
 		</div>
 	</section>
-
-<br><br>
-
-
-	<section id="holiday_type" class="holiday_type">
-		<div class="holiday_type_container">
-			<div class="widthcontainer">
-				<div><h3>Holidays Type</h3><h4>get explore your dream to travel the world!</h4></div>
-				<!-- <div class="widthcontainer"> -->
-					<div class="row">
-						<div class="col-6 col-md-4 col-lg-2 center">
-							<div class="holiday_type_content"><a href="#"><i class="fas fa-ship circle"></i><h4>Cruise</h4></a></div>
-						</div>
-						<div class="col-6 col-md-4 col-lg-2 center">
-							<div class="holiday_type_content"><a href="#"><i class="fas fa-city circle"></i><h4>City Breaks</h4></a></div>
-						</div>
-						<div class="col-6 col-md-4 col-lg-2 center">
-							<div class="holiday_type_content"><a href="#"><i class="fas fa-kiss-wink-heart circle"></i><h4>Honeymoon</h4></a></div>
-						</div>
-						<div class="col-6 col-md-4 col-lg-2 center">
-							<div class="holiday_type_content"><a href="#"><i class="far fa-compass circle"></i><h4>Adventure</h4></a></div>
-						</div>
-						<div class="col-6 col-md-4 col-lg-2 center">
-							<div class="holiday_type_content"><a href="#"><i class="fas fa-car circle"></i><h4>Safari</h4></a></div>
-						</div>
-						<div class="col-6 col-md-4 col-lg-2 center">
-							<div class="holiday_type_content"><a href="#"><i class="fas fa-umbrella-beach circle"></i><h4>Beach</h4></a></div>
-						</div>
-					</div>			
-				<!-- </div> -->
-			</div>
-		</div>
-	</section>
 <!-- Holiday type end -->
 <!-- Happy clients section start -->
 	<section id="clients" class="happy_clients">
@@ -359,180 +227,43 @@
 			<div class=""><h3>Happy Clients</h3>
 						<h4>what customer say about us and why love our services!</h4>
 			</div>
-			<div class="card-carousel">
-			  <div class="my-card">				
-			  	<div class="happy_clients_content">
+			<div class="flex_container6">
+				<div class="happy_clients_content">
 					<div class="happy_clients_content talk-bubble tri-right border round btm-left-in">
 						<a href="#"><h4>Lorem Ipsum is simply dummy text of the printing and typeset industry. Lorem Ipsum has been the industry's standard dummy text ever hen an with new version look.</h4>
 						<span>Client Name</span>
 						<h5>United States</h5></a>
 					</div>
-				</div></div>
-			  <div class="my-card">				
-			  	<div class="happy_clients_content">
+				</div>
+				<div class="happy_clients_content">
 					<div class="happy_clients_content talk-bubble tri-right border round btm-left-in">
-						<a href="#"><h4>Lorem Ipsum is simply dummy text of the printing and typeset industry. Lorem Ipsum has been the industry's standard dummy text ever hen an with new version look.</h4>
+						<h4>Lorem Ipsum is simply dummy text of the printing and typeset industry. Lorem Ipsum has been the industry's standard dummy text ever hen an with new version look.</h4>
 						<span>Client Name</span>
-						<h5>United States</h5></a>
+						<h5>United States</h5>
 					</div>
-				</div></div>
-			  <div class="my-card">				
-			  	<div class="happy_clients_content">
+				</div>
+				<div class="happy_clients_content">
 					<div class="happy_clients_content talk-bubble tri-right border round btm-left-in">
-						<a href="#"><h4>Lorem Ipsum is simply dummy text of the printing and typeset industry. Lorem Ipsum has been the industry's standard dummy text ever hen an with new version look.</h4>
+						<h4>Lorem Ipsum is simply dummy text of the printing and typeset industry. Lorem Ipsum has been the industry's standard dummy text ever hen an with new version look.</h4>
 						<span>Client Name</span>
-						<h5>United States</h5></a>
+						<h5>United States</h5>
 					</div>
-				</div></div>
-			  <div class="my-card">				
-			  	<div class="happy_clients_content">
+				</div>
+				<div class="happy_clients_content">
 					<div class="happy_clients_content talk-bubble tri-right border round btm-left-in">
-						<a href="#"><h4>Lorem Ipsum is simply dummy text of the printing and typeset industry. Lorem Ipsum has been the industry's standard dummy text ever hen an with new version look.</h4>
+						<h4>Lorem Ipsum is simply dummy text of the printing and typeset industry. Lorem Ipsum has been the industry's standard dummy text ever hen an with new version look.</h4>
 						<span>Client Name</span>
-						<h5>United States</h5></a>
+						<h5>United States</h5>
 					</div>
-				</div></div>
-			  <div class="my-card">			  	<div class="happy_clients_content">
-					<div class="happy_clients_content talk-bubble tri-right border round btm-left-in">
-						<a href="#"><h4>Lorem Ipsum is simply dummy text of the printing and typeset industry. Lorem Ipsum has been the industry's standard dummy text ever hen an with new version look.</h4>
-						<span>Client Name</span>
-						<h5>United States</h5></a>
-					</div>
-				</div></div>
-			  <div class="my-card">			  	<div class="happy_clients_content">
-					<div class="happy_clients_content talk-bubble tri-right border round btm-left-in">
-						<a href="#"><h4>Lorem Ipsum is simply dummy text of the printing and typeset industry. Lorem Ipsum has been the industry's standard dummy text ever hen an with new version look.</h4>
-						<span>Client Name</span>
-						<h5>United States</h5></a>
-					</div>
-				</div></div>
-			  <div class="my-card">
-			  	 	<div class="happy_clients_content">
-					<div class="happy_clients_content talk-bubble tri-right border round btm-left-in">
-						<a href="#"><h4>Lorem Ipsum is simply dummy text of the printing and typeset industry. Lorem Ipsum has been the industry's standard dummy text ever hen an with new version look.</h4>
-						<span>Client Name</span>
-						<h5>United States</h5></a>
-					</div>
-				</div></div>
+				</div>
 			</div>
-
-
-<!-- 			<div class="flex_container6">
-				<div class="happy_clients_content">
-					<div class="happy_clients_content talk-bubble tri-right border round btm-left-in">
-						<a href="#"><h4>Lorem Ipsum is simply dummy text of the printing and typeset industry. Lorem Ipsum has been the industry's standard dummy text ever hen an with new version look.</h4>
-						<span>Client Name</span>
-						<h5>United States</h5></a>
-					</div>
-				</div>
-				<div class="happy_clients_content">
-					<div class="happy_clients_content talk-bubble tri-right border round btm-left-in">
-						<h4>Lorem Ipsum is simply dummy text of the printing and typeset industry. Lorem Ipsum has been the industry's standard dummy text ever hen an with new version look.</h4>
-						<span>Client Name</span>
-						<h5>United States</h5>
-					</div>
-				</div>
-				<div class="happy_clients_content">
-					<div class="happy_clients_content talk-bubble tri-right border round btm-left-in">
-						<h4>Lorem Ipsum is simply dummy text of the printing and typeset industry. Lorem Ipsum has been the industry's standard dummy text ever hen an with new version look.</h4>
-						<span>Client Name</span>
-						<h5>United States</h5>
-					</div>
-				</div>
-				<div class="happy_clients_content">
-					<div class="happy_clients_content talk-bubble tri-right border round btm-left-in">
-						<h4>Lorem Ipsum is simply dummy text of the printing and typeset industry. Lorem Ipsum has been the industry's standard dummy text ever hen an with new version look.</h4>
-						<span>Client Name</span>
-						<h5>United States</h5>
-					</div>
-				</div>
-			</div> -->
 		<!-- </div> -->
 	</section>
 <!-- Happy clients section stop -->
 <!-- FOOTER start -->
 	<footer id="contacs" class="footer_on">
 		<div>
-			<div class="widthcontainer">
-					<div class="row">
-						<div class="col-12 col-md-6 col-lg-3 ">
-							<div class="footer_content">
-								<div><h3>Newsletter</h3></div>
-								<div><h4>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore.</h4>
-								<input type="text" id="newsletter" name="newletter" value="" placeholder="Subscribes...">
-								<span class="search_content btn">Go</span></div>
-							</div>
-						</div>
-						<div class="col-12 col-md-6 col-lg-3 ">
-							<div class="footer_content">
-								<div><h3>Latest News</h3></div>
-								<div class="latest_news_content">
-									<div class="flex_container_latest_news">
-										<div><img src="assets/images/offer1.jpg" alt="USA"></div>
-										<div class="flex_container_latest_news_msg"><h4>Postformat Gallery: Multiple images <span><br>October 21, 2013 - 3:31 pm</span></h4></div>
-									</div>
-									<div class="flex_container_latest_news">
-										<div><img src="assets/images/offer1.jpg" alt="USA"></div>
-										<div class="flex_container_latest_news_msg"><h4>Postformat Gallery: Multiple images <span><br>October 21, 2013 - 3:31 pm</span></h4></div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-12 col-md-6 col-lg-3 ">
-							<div class="footer_content">
-								<div><h3>Tags</h3></div>
-								<div class="">
-										<ul class="flex-container_tags wrap">
-										  <li class="flex-item"><a class="tags" href="#">Agent Login</a></li>
-										  <li class="flex-item"><a class="tags" href="#">Customer Login</a></li>
-										  <li class="flex-item"><a class="tags" href="#">Not a Member?</a></li>
-										  <li class="flex-item"><a class="tags" href="#">Contact</a></li>
-										  <li class="flex-item"><a class="tags" href="#">New Horozons</a></li>
-										  <li class="flex-item"><a class="tags" href="#">Lanscape</a></li>
-										  <li class="flex-item"><a class="tags" href="#">Tags</a></li>
-										  <li class="flex-item"><a class="tags" href="#">Nice</a></li>
-										  <li class="flex-item"><a class="tags" href="#">Some</a></li>
-										  <li class="flex-item"><a class="tags" href="#">Portrait</a></li>
-										</ul>
-								</div>
-							</div>
-						</div>
-						<div class="col-12 col-md-6 col-lg-3 ">
-							<div class="footer_content">
-								<div><h3>Address</h3></div>
-								<div class=""><h4>DieSachbearbeiter Schönhauser Allee 167c,10435 Berlin Germany 
-									<span>E-mail:</span><a href="mailto:info@yoursite.com"> moin@generator.de</h4></a>
-									<ul class="social flex-container">
-									<li>
-										<a href="http://facebook.com" target="_blank">
-											<i class="fab fa-facebook-f"></i>
-										</a>
-									</li>
-									<li>
-										<a href="http://pinterest.com" target="_blank">
-											<i class="fab fa-pinterest-p"></i>
-										</a>
-									</li>
-									<li>
-										<a href="http://twitter.com" target="_blank">
-											<i class="fab fa-twitter"></i>
-										</a>
-									</li>
-									<li>
-										<a href="http://linkedin.com" target="_blank">
-											<i class="fab fa-linkedin-in"></i>
-										</a>
-									</li>
-								</ul>
-								</div>
-							</div>
-						</div>
-					</div>	
-
-
-
-
-
+			<div class="container">
 				<div class="flex_container7">
 					<div class="footer_content">
 						<div><h3>Newsletter</h3></div>
@@ -603,7 +334,7 @@
 			<!-- </div> -->
 		</div>
 		<div class="footer_nav_top_border">
-			<div class="widthcontainer">
+			<div class="container">
 				<div>
 					<div class="footer_nav">
 						<ul class="flex-container">                        
@@ -623,6 +354,7 @@
 	</footer>
 <!-- FOOTER stop -->
 <!-- jQuery library -->
+<<<<<<< HEAD
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- 		<script src="https://code.jquery.com/jquery-3.3.1.min.js" 
         integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" 
@@ -633,5 +365,9 @@
 		<!-- <script src="assets/js/bootstrap.min.js"></script> -->
 		<script type="text/javascript" src="assets/js/carousel.js"></script>
 		<script type="text/javascript" src="assets/js/custom.js"></script>
+=======
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/custom.js"></script>
+>>>>>>> parent of aed4d65... karusele
 </body>
 </html>
